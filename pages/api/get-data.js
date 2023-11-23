f7740a2dc849931eb5d3491855b03a312df90f69
isabellaes <borjessonisabella@gmail.com>
2023-11-23 13:55:25 +0100

    back to original

--- pages/api/get-data.js
+++
@@ -1,6 +0,0 @@
-import { sql } from "@vercel/postgres";
-
-export default async function handler(response) {
-  const pellets = await sql`SELECT * FROM pellets;`;
-  return response.status(200).json({ pellets });
-}
